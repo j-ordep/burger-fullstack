657c852f948ae36fdd9ef21e25ddc24daf0864fb
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-25 18:28:25 -0300

    feat: add db connection e queries

--- backend/db/queries.sql
+++ backend/db/queries.sql
@@ -1,3 +1,4 @@
+-- Active: 1755963471787@@localhost@5432@burger
 CREATE TABLE paes (
     id SERIAL PRIMARY KEY,
     tipo VARCHAR(50) NOT NULL
@@ -18,16 +19,39 @@ CREATE TABLE opcionais (
     tipo VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE burger_opcionais (
-    burger_id INTEGER REFERENCES burgers(id) ON DELETE CASCADE,
-    opcional_id INTEGER REFERENCES opcionais(id),
-    PRIMARY KEY (burger_id, opcional_id)
-);
-
 CREATE TABLE burgers (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    pao VARCHAR(50) NOT NULL,
-    carne VARCHAR(50) NOT NULL,
-    status VARCHAR(50) NOT NULL
+    nome_cliente TEXT NOT NULL,
+    pao_id INT REFERENCES paes(id),
+    carne_id INT REFERENCES carnes(id),
+    status_id INT REFERENCES status(id),
+    created_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE TABLE burger_opcionais (
+    burger_id INTEGER,
+    opcional_id INTEGER,
+    PRIMARY KEY (burger_id, opcional_id),
+    FOREIGN KEY (burger_id) REFERENCES burgers(id) ON DELETE CASCADE,
+    FOREIGN KEY (opcional_id) REFERENCES opcionais(id)
+);
+
+INSERT INTO paes (id, tipo) VALUES
+(1, 'Italiano Branco'),
+(2, '3 Queijos'),
+(3, 'Parmesão e Orégano'),
+(4, 'Integral');
+
+INSERT INTO carnes (id, tipo) VALUES
+(1, 'Maminha'),
+(2, 'Alcatra'),
+(3, 'Picanha'),
+(4, 'Veggie burger');
+
+INSERT INTO opcionais (id, tipo) VALUES
+(1, 'Bacon'),
+(2, 'Cheddar'),
+(3, 'Salame'),
+(4, 'Tomate'),
+(5, 'Cebola roxa'),
+(6, 'Pepino');

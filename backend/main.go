a69d7eec1553e98e7526dbe95a392570d64902c5
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-25 18:27:10 -0300

    feat: add rotas - main

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"backend-super-burger/Handler"
+	"backend-super-burger/config"
+	"backend-super-burger/db"
+	"backend-super-burger/repository"
+	"backend-super-burger/service"
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
 func main() {
+	err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 	
+	db, err := db.OpenConnection()
+	if err != nil {
+		log.Fatal("Erro ao conectar no banco:", err)
+	}
+	defer db.Close()
+
+	ingredientesRepo := repository.NewIngredientesRepository(db)
+	ingredientesService := service.NewIngredientesService(ingredientesRepo)
+	ingredientesHandler := handler.NewIngredientesHandler(ingredientesService)
+
+	statusRepo := repository.NewStatusRepository(db)
+	statusService := service.NewStatusService(statusRepo)
+	statusHandler := handler.NewStatusHandler(statusService)
+
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+    e.Use(middleware.CORS())
+
+	e.GET("/ingredientes", ingredientesHandler.GetIngredientes)
+	e.GET("/status", statusHandler.GetStatus)
 }

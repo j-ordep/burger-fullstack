5ee2870f54790b9b55d0725cc0868a439483adf3
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-27 23:34:43 -0300

    feat: add GetBurgers, DeleteBurgerById e metodos auxiliares - BurgerService

--- backend/service/BurgerService.go
+++ backend/service/BurgerService.go
@@ -12,10 +12,49 @@ func NewBurgerService(repo domain.BurgerRepository) *BurgerService {
 	return &BurgerService{repo: repo}
 }
 
-func (s *BurgerService) SaveBurger(burger *domain.Burger) error {
-	err := s.repo.SaveBurger(burger)
+func (s *BurgerService) SaveBurger(burger *domain.Burger) (int, error) {
+	burgerId, err := s.repo.SaveBurger(burger)
+	if err != nil {
+		return 0, err
+	}
+	return burgerId, nil
+}
+
+func (s *BurgerService) GetBurgers() ([]*domain.Burger, error) {
+	burgers, err := s.repo.GetBurgers()
+	if err != nil {
+		return nil, err
+	}
+
+	return burgers, nil
+}
+
+func (s *BurgerService) DeleteBurgerById(id int) (error) {
+	err := s.repo.DeleteBurger(id)
 	if err != nil {
 		return err
 	}
+
 	return nil
+}
+
+// funções auxiliares
+
+
+func (s *BurgerService) GetStatusIdByName(status string) (int, error) {
+	statusId, err := s.repo.GetStatusIdByName(status)
+	if err != nil {
+		return 0, err
+	}
+
+	return statusId, nil
+}
+
+func (s *BurgerService) GetStatusTypeById(statusId int) (string, error) {
+	statustype, err := s.repo.GetStatusTypeById(statusId)
+	if err != nil {
+		return "", err
+	}
+
+	return statustype, nil
 }

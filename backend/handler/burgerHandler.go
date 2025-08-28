ac70bbe6038d2ef543bf6aae0f157391a4a5f3b1
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-27 23:35:34 -0300

    feat: add GetBurgers, DeleteBurgerById e metodos auxiliares - BurgerHandler

--- backend/handler/burgerHandler.go
+++ backend/handler/burgerHandler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
-	"backend-super-burger/domain"
+	"backend-super-burger/dto"
 	"backend-super-burger/service"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -17,19 +19,77 @@ func NewBurgerHandler(service *service.BurgerService) *BurgerHandler {
 }
 
 func (h *BurgerHandler) SaveBurger(c echo.Context) error {
-	var burger *domain.Burger 
-	err := c.Bind(&burger)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, map[string]string {
-			"error" : "JSON inválido",
-		})
-	} 
+	var burgerDto dto.Input
 
-	err = h.service.SaveBurger(burger)
+	err := c.Bind(&burgerDto)
 	if err != nil {
-		return err
+		c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "JSON inválido",
+		})
 	}
 
-	return c.JSON(http.StatusOK, nil)
+	// o input (burgerDto) vem com status em fomato de string, logo passei essa func para buscar no db qual id do status (exemplo: id:1, tipo:"Solicitado")
+	statusId, err := h.service.GetStatusIdByName(burgerDto.Status)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"error": "status_id não encontrado",
+		})
+	}
 
+	burger := burgerDto.ToDomain(statusId)
+
+	burgerId, err := h.service.SaveBurger(burger)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"error": "impossivel adicionar pedido",
+		})
+	}
+
+	return c.JSON(http.StatusOK, burgerId)
+}
+
+func (h *BurgerHandler) GetBurgers(c echo.Context) error {
+	burgers, err := h.service.GetBurgers()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"error": "impossível listar pedidos",
+		})
+	}
+
+	var outputDTOs []*dto.Output
+
+	for _, burger := range burgers {
+		statusType, err := h.service.GetStatusTypeById(burger.StatusId)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{
+				"error": "erro ao processar dados dos pedidos",
+			})
+		}
+
+		outputDTO := dto.FromDomain(burger, statusType)
+		outputDTOs = append(outputDTOs, outputDTO)
+	}
+
+	return c.JSON(http.StatusOK, outputDTOs)
+}
+
+func (h *BurgerHandler) DeleteBurger(c echo.Context) error {
+	idParam := c.Param("id")
+
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "ID inválido",
+		})
+	}
+
+	if err := h.service.DeleteBurgerById(id); err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"error": "Erro ao deletar o pedido",
+		})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"message": fmt.Sprintf("Pedido com id %s deletado com sucesso", id),
+	})
 }
